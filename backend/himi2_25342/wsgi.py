d23e58bb262281065ffb98cf8865263a7db39b6d
Crowdbotics <team@crowdbotics.com>
2021-03-29 13:21:34 +0000

    Initial commit.

---
+++ backend/himi2_25342/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for himi2_25342 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'himi2_25342.settings')
+
+application = get_wsgi_application()
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://himi2-25342.botics.co",
+  defaultTimeout: 5000
+};
